f41c4f026a7285755e4cbc4122bcf095a3a7c77f
v.naydenko <v.naydenko@VNAYDENKO>
2024-10-23 16:33:48 +0500

    Add Room Core/Dto/Model/Rooms

--- Core/DTOs/Models/Room.cs
+++ Core/DTOs/Models/Room.cs
@@ -1,7 +1,34 @@
-﻿namespace Core.DTOs.Models
+﻿using Data.Enums;
+
+namespace Core.DTOs.Models
 {
     public class Room
     {
+
+        public class RoomResponseDto
+        {
+            public long Id { get; set; }
+            public string Number { get; set; }
+            public RoomTypes RoomType { get; set; }
+            public decimal Price { get; set; }
+        }
+
+
+        public class CreateRoomDto // TODO: ID не принимаем назначается базой
+        {
+            public string Number { get; set; }
+            public RoomTypes RoomType { get; set; } // TODO: Ссылка на Data.Enums Нужно ли отдельный енам создавать?
+            public decimal Price { get; set; }
+        }
+
+
+        public class UpdateRoomDto
+        {
+            public long Id { get; set; }
+            public string Number { get; set; }
+            public RoomTypes RoomType { get; set; }
+            public decimal Price { get; set; }
+        }
 
     }
 }
